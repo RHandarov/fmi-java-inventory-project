e061d9f4d31efa5d49903c4b626c7efed2ab4c6d
Radostin Handarov <radostin.handarov@gmail.com>
2025-04-03 19:55:26 +0300

    Add more methods to dummy controller

--- src/main/java/fmi/java/inventory_project/controller/DummyController.java
+++ src/main/java/fmi/java/inventory_project/controller/DummyController.java
@@ -1,6 +1,9 @@
 package fmi.java.inventory_project.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/dummy")
 public class DummyController {
     @GetMapping("/hello-world")
-    public String sayHelloWorld() {
-        return "Hello World from dummy controller";
+    public ResponseEntity<String> sayHelloWorld() {
+        return new ResponseEntity<String>("Hello World from dummy controller",
+                HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/hello/{name}")
+    public String sayHelloToPerson(@PathVariable("name") String name) {
+        return "Hello, " + name;
     }
 }
